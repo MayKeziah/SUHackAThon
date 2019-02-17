538b7a68dac03501eb3e7d9536a409c520be7244
MayKeziah <45299665+MayKeziah@users.noreply.github.com>
2019-02-17 14:11:23 -0800

    copy-paste to rankMe

---
+++ FillMySchedule/src/schedule/RankMe.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package schedule;
+
+/**
+ * @author E
+ *
+ */
+public class RankMe {
+	public static int getEventRank(String[] eventKeywords, String[] topTags) {
+		int countScore = 0;
+		for (int i = 0; i < topTags.length; i++) { //i == index of user's topTags
+			for (int j = 0; j < eventKeywords.length; j++) { //j == index of this events keywords
+				if(eventKeywords[j].equals(topTags[i])) {
+					countScore++;
+				}
+			}
+		}
+		
+		return countScore;
+	}
+	
+	public static Event[] getTopSuggestions(Event[] eventOptions, String[] topTags) { //ranks events and populates a top-suggestions Event[7]
+		int[] eventIndexRank = new int[eventOptions.length];
+		int topRankIndex = 0;
+		int topRankValue = 0;
+		Event[] suggestThese = new Event[7];
+		for (int i = 0; i < eventOptions.length; i++) {
+			eventIndexRank[i] = getEventRank(eventOptions[i].getKeyWords(), topTags);
+			if (eventIndexRank[i] > topRankValue) {
+				topRankValue = eventIndexRank[i];
+				topRankIndex = i;
+			}
+		}
+		suggestThese[0] = eventOptions[topRankIndex];
+		eventIndexRank[topRankIndex] = 0;
+		for (int j = 1; j < 7; j++) {
+			int index = findMax(eventIndexRank);
+			suggestThese[j] = eventOptions[index];
+			eventIndexRank[index] = -1; //here
+		}
+		return suggestThese;
+	}
+	
+	public static int findMax(int[] list) {
+		int currentMax = list[0];
+		int maxIndex = 0;
+		for(int i = 0; i < list.length; i++) {
+			if(list[i] >= currentMax) {
+				currentMax = list[i];
+				maxIndex = i;
+			}
+		}
+		return maxIndex;
+	}
+
+}
