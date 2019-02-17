f3eb0e81e545e1789b11a688ca951546e4aaae0b
MayKeziah <45299665+MayKeziah@users.noreply.github.com>
2019-02-17 14:32:53 -0800

    Finish RankMe

--- FillMySchedule/src/schedule/RankMe.java
+++ FillMySchedule/src/schedule/RankMe.java
@@ -3,15 +3,23 @@
  */
 package schedule;
 
+import java.util.ArrayList;
+
 /**
  * @author E
  *
  */
 public class RankMe {
 	private Event[] allEvents;
-	private Event[] topSuggestions;
+	private Event[] topSuggestions = new Event[7];
 	
-	
+	public RankMe() {}
+	public RankMe(ArrayList<Event> allEvents) {
+		this.allEvents = new Event[allEvents.size()];
+		for (int i = 0; i < allEvents.size(); i++) {
+			this.allEvents[i] = allEvents.get(i);
+		}
+	}
 	private int getEventRank(String[] eventKeywords, String[] topTags) {
 		int countScore = 0;
 		for (int i = 0; i < topTags.length; i++) { //i == index of user's topTags
@@ -25,23 +33,23 @@ public class RankMe {
 		return countScore;
 	}
 	
-	public Event[] getTopSuggestions(Event[] eventOptions, String[] topTags) { //ranks events and populates a top-suggestions Event[7]
-		int[] eventIndexRank = new int[eventOptions.length];
+	public Event[] getTopSuggestions(String[] topTags) { //ranks events and populates a top-suggestions Event[7]
+		int[] eventIndexRank = new int[allEvents.length];
 		int topRankIndex = 0;
 		int topRankValue = 0;
 		Event[] suggestThese = new Event[7];
-		for (int i = 0; i < eventOptions.length; i++) {
-			eventIndexRank[i] = getEventRank(eventOptions[i].getKeyWords(), topTags);
+		for (int i = 0; i < allEvents.length; i++) {
+			eventIndexRank[i] = getEventRank(allEvents[i].getKeyWords(), topTags);
 			if (eventIndexRank[i] > topRankValue) {
 				topRankValue = eventIndexRank[i];
 				topRankIndex = i;
 			}
 		}
-		suggestThese[0] = eventOptions[topRankIndex];
+		suggestThese[0] = allEvents[topRankIndex];
 		eventIndexRank[topRankIndex] = 0;
 		for (int j = 1; j < 7; j++) {
 			int index = findMax(eventIndexRank);
-			suggestThese[j] = eventOptions[index];
+			suggestThese[j] = allEvents[index];
 			eventIndexRank[index] = -1; //here
 		}
 		return suggestThese;
